cf20c7c6774abebce6f7798fc8bbab7c9826dbf9
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-02-11 20:10:41 +0100

    Crear col·leccions a localStorage: dades_tiquets i dades_usuaris. Implementar taules de "Tiquets Pendents" i "Tiquets Resolts".

--- src/views/Panel.jsx
+++ src/views/Panel.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import { inicializacionLocalStorage } from '../lib/funciones';
+
+inicializacionLocalStorage();
 
 const Panel = () => {
+
+    const [tickets, setTickets] = useState([]);
+
+    useEffect(() => {
+        const storedTickets = localStorage.getItem('dades_tiquets');
+        if (storedTickets) {
+          setTickets(JSON.parse(storedTickets));
+        }
+      }, []);
+
+    console.log(tickets);
+
   return (
     <>
-    {console.log(Header)}
         <Header/>
       <main className="container mt-5">
         <h1>Administración de incidencias</h1>
@@ -24,71 +39,22 @@ const Panel = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>123459</td>
-              <td>18/04/2023</td>
-              <td>T6</td>
-              <td>DAW1</td>
-              <td>PC3</td>
-              <td>Error de impresora</td>
-              <td>Ana Martínez</td>
-              <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
-              <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123460</td>
-              <td>19/04/2023</td>
-              <td>T8</td>
-              <td>DAW2</td>
-              <td>PC4</td>
-              <td>Problema de acceso a archivos</td>
-              <td>Pedro Gómez</td>
-              <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
-              <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123461</td>
-              <td>20/04/2023</td>
-              <td>T6</td>
-              <td>DAW1</td>
-              <td>PC1</td>
-              <td>Aplicación se cierra inesperadamente</td>
-              <td>Sofía Fernández</td>
-              <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
-              <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123462</td>
-              <td>21/04/2023</td>
-              <td>T7</td>
-              <td>DAW2</td>
-              <td>PC2</td>
-              <td>Problema de conexión a la red</td>
-              <td>Luis Torres</td>
-              <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
-              <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123463</td>
-              <td>22/04/2023</td>
-              <td>T8</td>
-              <td>DAW1</td>
-              <td>PC3</td>
-              <td>Archivos corruptos</td>
-              <td>Carolina Ramírez</td>
-              <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
-              <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
+            {
+                tickets.filter(ticket => ticket.estado === 'false').map((ticket) => (
+                    <tr key={ticket.codigo}>
+                    <td>{ticket.codigo}</td>
+                    <td>{ticket.fecha}</td>
+                    <td>{ticket.aula}</td>
+                    <td>{ticket.grupo}</td>
+                    <td>{ticket.ordenador}</td>
+                    <td>{ticket.descripcion}</td>
+                    <td>{ticket.alumno}</td>
+                    <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
+                    <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
+                    <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
+                    <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <h2 className="mt-5">Tickets resueltos</h2>
@@ -106,42 +72,21 @@ const Panel = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>123457</td>
-              <td>16/04/2023</td>
-              <td>15/05/2023</td>
-              <td>T7</td>
-              <td>DAW2</td>
-              <td>PC1</td>
-              <td>Problema de conexión a Internet</td>
-              <td>Maria López</td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123458</td>
-              <td>17/04/2023</td>
-              <td>15/05/2023</td>
-              <td>T8</td>
-              <td>DAW1</td>
-              <td>PC2</td>
-              <td>Pantalla en blanco</td>
-              <td>Juan Rodríguez</td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
-            <tr>
-              <td>123459</td>
-              <td>18/04/2023</td>
-              <td>15/05/2023</td>
-              <td>T8</td>
-              <td>DAW1</td>
-              <td>PC3</td>
-              <td>Error de impresora</td>
-              <td>Ana Martínez</td>
-              <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-              <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
-            </tr>
+          {
+            tickets.filter(ticket => ticket.estado === 'true').map((ticket) => (
+                <tr key={ticket.codigo}>
+                    <td>{ticket.codigo}</td>
+                    <td>{ticket.fecha}</td>
+                    <td>{ticket.fechaResuelto}</td>
+                    <td>{ticket.aula}</td>
+                    <td>{ticket.grupo}</td>
+                    <td>{ticket.ordenador}</td>
+                    <td>{ticket.descripcion}</td>
+                    <td>{ticket.alumno}</td>
+                    <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
+                    <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
+                </tr>
+            ))}
           </tbody>
         </table>
       </main>
