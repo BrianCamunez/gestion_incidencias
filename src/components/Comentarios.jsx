e0515243ad985bd332b69ffe81d8155871eef6ee
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-03-18 18:06:49 +0100

    Cambio de mayuscula para el despliege

--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Comentario } from './comentario';
+import { Comentario } from './Comentario';
 import { useParams } from 'react-router-dom';
 
 
